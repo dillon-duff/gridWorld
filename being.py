93cee552720709a7d9b30670bb1160a9adf4d386
Dillon2332 <duffman2332@gmail.com>
2024-11-12 20:54:45 -0500

    being brainstorm

---
+++ being.py
@@ -0,0 +1,23 @@
+# Contains colors/cell types and coordinates based on the relative 0,0 being the top left corner of the 'being'
+
+# Contains a neural network brain that receives inputs and outputs actions
+
+# Energy can be consumed and is consumed proportional to size, brain size/usage, and actions
+
+# Can interact with other beings
+
+# Can consume food to gain energy
+
+# Can reproduce with another being
+
+# Cell types (colors)
+# Green: Move forward
+# Red: Consume
+# Blue: Rotate
+# Gray: Do nothing
+# Brown: Shield
+# Yellow: Sensor? Eye? Vision cone?
+# Purple: Reproduce
+
+
+
